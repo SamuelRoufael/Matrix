4560223ce4ac73cfe539684da1b099d674382e7f
omar-gamal99 <omar99_mohamed@hotmail.com>
2021-11-09 23:31:25 +0200

    first commit

--- src/code/Node.java
+++ src/code/Node.java
@@ -1,7 +1,5 @@
 package code;
 
-import com.sun.xml.internal.bind.v2.TODO;
-
 public class Node {
 	private static int numberOfNodes;
 	private Node parentNode;
@@ -30,5 +28,91 @@ public class Node {
 	public String removeObject(short x, short y) {
 		// TODO remove Agent, Hostage, Pill, Mutated hostage
 		return "";
+	}
+
+	public static String[] extractGridSize(String state){
+		String [] array  = state.split(";");
+		String sizeGrid = array[0];
+		String [] gridSize = sizeGrid.split(",",1);
+		return gridSize;
+	}
+
+	public static String[] extractMaxNoOfCarry(String state){
+		String [] array = state.split(";");
+		String [] MaxCarryNo = new String[1];
+		MaxCarryNo[0] = array[1];
+		return MaxCarryNo;
+	}
+
+	public static String[] extractNeoPos(String state){
+		String [] array = state.split(";");
+		String posNeo = array[2];
+		String [] neoPos = posNeo.split(",");
+		return neoPos;
+	}
+
+	public static String[] extractTelBoothPos(String state){
+		String [] array = state.split(";");
+		String telPos = array[3];
+		String [] telBoothPos = telPos.split(",");
+		return telBoothPos;
+	}
+
+	public static String[] extractAgentsPos(String state){
+		String [] array = state.split(";");
+		String posAgents = array[4];
+		String [] agentsPos = posAgents.split(",");
+		return agentsPos;
+	}
+
+	public static String[] extractPillPos(String state){
+		String [] array = state.split(";");
+		String posPills = array[5];
+		String [] pillPos = posPills.split(",");
+		return pillPos;
+	}
+
+	public static String[] extractPadPos(String state){
+		String [] array = state.split(";");
+		String posPad = array[6];
+		String [] padPos = posPad.split(",");
+		return padPos;
+	}
+
+	public static String[] extractHostagesPos(String state){
+		String [] array = state.split(";");
+		String posHos = array[7];
+		String [] hosPos = posHos.split(",");
+		return hosPos;
+	}
+
+	public static String[] extractCarriedHostagesHP(String state){
+		String [] array = state.split(";");
+		String hpHos = array[8];
+		String [] carriedHosHP = hpHos.split(",");
+		return carriedHosHP;
+	}
+
+	public static String[] extractMutatedHostagesPos(String state){
+		String [] array = state.split(";");
+		String posMutatedHos = array[9];
+		String [] mutatedHosPos = posMutatedHos.split(",");
+		return mutatedHosPos;
+	}
+
+	public static boolean goalTest(String state){
+		String [] neoPosDam = extractNeoPos(state);
+		String [] neoPos = new String[2];
+		neoPos[0] = neoPosDam[0];
+		neoPos[1] = neoPosDam[1];
+		String [] telBoothPos = extractTelBoothPos(state);
+		String [] hostages = extractHostagesPos(state);
+		String [] mutatedHostages = extractMutatedHostagesPos(state);
+		if(neoPos.equals(telBoothPos) && hostages == null && mutatedHostages == null){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
